3a08910c154fe5ca90776462dcdd0a7250fd5b74
chia-chih <d06921014@ntu.edu.tw>
2022-06-10 02:36:31 +0800

    add data preparation

---
+++ dataPreparation.py
@@ -0,0 +1,24 @@
+import os
+import pdb
+import numpy as np
+import apuf_lib as ap
+import utils
+   
+length = 128
+seed = 42
+nchal = 500000
+
+dataDir = os.path.join(os.getcwd(), "dataset/dataset-{}bit-challenge".format(length))
+
+print(dataDir)
+if not os.path.exists(dataDir):
+    os.makedirs(dataDir)
+
+filename = "dataset-{}.csv".format(nchal)
+
+chs = utils.genNChallenges(length, nchal)
+
+print("save challenges...")
+np.savetxt(os.path.join(dataDir, filename), chs, fmt="%d", delimiter=',')
+
+#pdb.set_trace()
